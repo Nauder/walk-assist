ce3530d5e6b7f2877a08f83eab6f930e8825c87b
Nauder <prestes.nauder@gmail.com>
2023-12-09 16:37:05 -0300

    Add Session Sync log

--- blueprints/session_workshop.py
+++ blueprints/session_workshop.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 
 from exceptions import InvalidFieldException
 from services.session_service import get_sessions
@@ -9,12 +9,15 @@ session_mold = Blueprint("sessions", __name__)
 
 @session_mold.get('')
 def get_sessions_route() -> Response:
-    try:
-        return build_response(
-            True,
-            "sessions obtained successfully",
-            200,
-            kwargs={'sessions': get_sessions()}
-        )
-    except InvalidFieldException as ex:
-        return build_response(False, str(ex), 422)
+    if request.args['api_key'] == 'api123':
+        try:
+            return build_response(
+                True,
+                "sessions obtained successfully",
+                200,
+                kwargs={'sessions': get_sessions()}
+            )
+        except InvalidFieldException as ex:
+            return build_response(False, str(ex), 422)
+    else:
+        return build_response(False, "user not authorized", 403)
--- resources/js/session_watcher.js
+++ resources/js/session_watcher.js
@@ -1,6 +1,16 @@
+function getCurrentDate() {
+    const currentDate = new Date();
+    return "Last Sync: " + currentDate.getDate() + "/"
+        + (currentDate.getMonth() + 1) + "/"
+        + currentDate.getFullYear() + " @ "
+        + currentDate.getHours() + ":"
+        + currentDate.getMinutes() + ":"
+        + currentDate.getSeconds();
+}
+
 function getSessions() {
     $.get({
-        url: "http://localhost:22000/sessions",
+        url: "http://localhost:22000/sessions?api_key=api123",
         success: function (result) {
             const tableBody = $("#sessionTableBody");
             tableBody.empty();
@@ -13,11 +23,14 @@ function getSessions() {
                                </tr>`;
                 tableBody.append(row);
             });
+            $("#error").text('');
         },
         error: function (error) {
-            console.log(`Error ${error}`);
+            $("#error").text('Error obtaining sessions');
+            console.error(error);
         }
     });
+    $("#info").text(getCurrentDate());
 }
 
 $(document).ready(function () {
